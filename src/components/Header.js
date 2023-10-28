5c697fbcecb927f4eab4ad677e32fc22d0b55515
vivekpanth <vivekpanth09@gmail.com>
2023-10-28 22:05:25 +0530

    Updated profile and Implemented Sign Out

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,34 @@
+import { signOut } from "firebase/auth";
 import React from "react";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-  return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+
+    const user=useSelector(store=>store.user);
+  const navigate=  useNavigate()
+    const handleSignOut=()=>{
+        signOut(auth).then(() => {
+            navigate("/")
+            // Sign-out successful.
+          }).catch((error) => {
+            // An error happened.
+            navigate("/error")
+          });
+          
+    }
+  return ( 
+    <div className="absolute  flex justify-between w-screen px-8 py-2 bg-gradient-to-b from-black z-10">
       <img className="w-44"
 src="https://images.ctfassets.net/y2ske730sjqp/6bhPChRFLRxc17sR8jgKbe/6fa1c6e6f37acdc97ff635cf16ba6fb3/Logos-Readability-Netflix-logo.png "alt="Logo"
       />
+      {user&&(<div className="flex  text-center">
+        <img className=" h-12 pt-4 "src={user?.photoURL}/>
+      <button onClick={handleSignOut} className="pb-10 text-white font-bold cursor-pointer pt-2">(Sign Out)</button>
+      </div>)}
     </div>
+    
   );
 };
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,20 @@
 import React, { useRef, useState } from "react";
 import Header from "./Header";
 import { checkValidData } from "../utils/validate";
-import {  createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {  createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, seterrorMessage] = useState(null);
+   const dispatch=useDispatch();
+  const navigate=useNavigate()
 
-
-
+   const name=useRef(null)
    const email=useRef(null)
    const password=useRef(null)
   const toggleSignInForm = () => {
@@ -25,6 +29,19 @@ const Login = () => {
         .then((userCredential) => {
           // Signed up 
           const user = userCredential.user;
+          updateProfile(auth.currentUser, {
+            displayName: name.current.value, photoURL:"https://avatars.githubusercontent.com/u/106002167?v=4"
+          }).then(() => {
+            const {uid,email,displayName,photoURL} = auth.currentUser;
+          dispatch(addUser({uid:uid,email:email,displayName:displayName,photoURL:photoURL}))
+            // Profile updated!
+            navigate("/browse")
+            
+          }).catch((error) => {
+            seterrorMessage(error.message)
+            // An error occurred
+            // ...
+          });
           console.log(user)
           // ...
         })
@@ -32,6 +49,7 @@ const Login = () => {
           const errorCode = error.code;
           const errorMessage = error.message;
           seterrorMessage(errorCode+"-"+errorMessage)
+          navigate("/")
           // ..
         });
        }
@@ -72,7 +90,7 @@ const Login = () => {
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
         {!isSignInForm && 
-          <input
+          <input ref={name}
             type="text"
             placeholder="Full Name"
             className="p-4 my-4 w-full bg-gray-600"
